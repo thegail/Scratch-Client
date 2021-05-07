75098c5c2156c9123815070a919e9ca698e936da
Theodore Gaillard <teddy.gaillard@icloud.com>
2021-05-06 17:22:06 -0700

    Initial Commit

---
+++ Scratch Client/Scratch_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scratch_ClientApp.swift
+//  Scratch Client
+//
+//  Created by Teddy Gaillard on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Scratch_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
